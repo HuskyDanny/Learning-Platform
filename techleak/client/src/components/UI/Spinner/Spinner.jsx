c55d785d6123a3b7bbe635a6c3d233a1546ce9c6
Junchen Pan <junchenp1018@gmail.com>
2019-07-09 14:48:03 +0800

    Add a loader

--- techleak/client/src/components/UI/Spinner/Spinner.jsx
+++ techleak/client/src/components/UI/Spinner/Spinner.jsx
@@ -1,6 +1,16 @@
-import React from "react";
-const Spinner = props => {
-  return <div className="loader"> I am trying...</div>;
-};
-
-export default Spinner;
+import React, { Component } from "react";
+import Loader from "react-loader-spinner";
+export default class Spinner extends React.Component {
+  //other logic
+  render() {
+    return (
+      <Loader
+        style={{ margin: "10% 10%" }}
+        type="ThreeDots"
+        color="#00BFFF"
+        height="100"
+        width="100"
+      />
+    );
+  }
+}
