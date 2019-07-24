e17f49343eb5ee45c372238b30b16b0244ac9173
Garret Yang <GarretYang>
2019-07-24 09:57:37 +0800

    fixed contact us email. fixed display in userprofile when the window is shrinking

--- techleak/server/routes/api/contact/contact.js
+++ techleak/server/routes/api/contact/contact.js
@@ -1,31 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var nodemailer = require('nodemailer');
-const creds = require('../../../config/contactEmail');
-
-var transport = {
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
-  auth: {
-    type: 'oauth2',
-    user: creds.USER,
-    clientId: creds.CID,
-    clientSecret: creds.CIS,
-    refreshToken: creds.RST,
-    accessToken: creds.ATK,
-  }
-}
-
-var transporter = nodemailer.createTransport(transport)
-
-transporter.verify((error, success) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Server is ready to take messages');
-  }
-});
+const sgMail = require("../../../config/sgMail");
 
 router.post("/contact", (req, res, next) => {
   var firstname = req.body.firstname
@@ -34,26 +9,15 @@ router.post("/contact", (req, res, next) => {
   var phone = req.body.phone
   var title = req.body.title
   var message = req.body.message
-  var content = `firstname: ${firstname} \n familyname: ${familyname} \n email: ${email} \n phone: ${phone} \n message: ${message}`
+  var content = `Firstname: ${firstname} \n Familyname: ${familyname} \n Email: ${email} \n Phone: ${phone} \n \n Message: ${message}`
 
-  var mail = {
-    from: firstname,
-    to: "exploreprograming@gmail.com",  //Change to email address that you want to receive messages on
-    subject: `[ContactUs Message]: ${title}`,
-    text: content,
-  }
-
-  transporter.sendMail(mail, (err, data) => {
-    if (err) {
-      res.json({
-        msg: 'fail'
-      })
-    } else {
-      res.json({
-        msg: 'success'
-      })
-    }
-  })
+  const msg = {
+    to: "exploreprograming@gmail.com",
+    from: "contactUs@techleak.com",
+    subject: "[Contact Us Message] " + title,
+    text: content
+  };
+  sgMail.send(msg);
 })
 
 module.exports = router;
