29d6aa7d347676b5a6a2c6d3efa0d147b2ad55af
Garret Yang <GarretYang>
2019-07-31 16:31:20 +0800

    add contact us notification for user. remove unused files

--- techleak/server/routes/api/contact/contact.js
+++ techleak/server/routes/api/contact/contact.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const sgMail = require("../../../config/sgMail");
 
-router.post("/contact", (req, res, next) => {
+router.post("/contact", async (req, res, next) => {
   var firstname = req.body.firstname
   var familyname = req.body.familyname
   var email = req.body.email
@@ -10,14 +10,21 @@ router.post("/contact", (req, res, next) => {
   var title = req.body.title
   var message = req.body.message
   var content = `Firstname: ${firstname} \n Familyname: ${familyname} \n Email: ${email} \n Phone: ${phone} \n \n Message: ${message}`
-
-  const msg = {
-    to: "exploreprograming@gmail.com",
-    from: "contactUs@techleak.com",
-    subject: "[Contact Us Message] " + title,
-    text: content
-  };
-  sgMail.send(msg);
+  var result;
+  try {
+    const msg = {
+      to: "exploreprograming@gmail.com",
+      from: "contactUs@techleak.com",
+      subject: "[Contact Us Message] " + title,
+      text: content
+    };
+    await sgMail.send(msg);
+    result = res.send(JSON.stringify({ msg: 'success' }));
+  } catch (error) {
+    console.log(error);
+    result = res.send(JSON.stringify({ msg: 'fail' }));
+  }
+  return result;
 })
 
 module.exports = router;
