a905cbe6c805af28de145250e8ef4aa18a86bb5b
HANTAO LIU <hantal@uw.edu>
2019-08-06 21:57:58 +0800

    limit the tags to 1 to 3

--- techleak/client/src/components/publish/Publish.jsx
+++ techleak/client/src/components/publish/Publish.jsx
@@ -2,7 +2,7 @@ import axios from "../../axios-blogs";
 import React, { Component } from "react";
 import Editor from "../editor/Editor";
 import { Link } from "react-router-dom";
-import image from "../../assets/img/logo.jpg";
+import image from "../../assets/img/logo1.jpg";
 import DropDown from "../dropdown/dropdown";
 import Spinner from "../UI/Spinner/Spinner";
 import Modal from "react-responsive-modal";
@@ -27,60 +27,75 @@ class Publish extends Component {
       posted: false,
       content: "",
       title: "",
-      loading: false
+      loading: false,
+      tagError: false
     };
 
-    this.handlePost = this.handlePost.bind(this);
+    this.handlePostCheck = this.handlePostCheck.bind(this);
+    this.handleFinalPost = this.handleFinalPost.bind(this);
     this.successPosted = this.successPosted.bind(this);
     this.updateContent = this.updateContent.bind(this);
   }
 
-  handlePost = async () => {
+  handleFinalPost = async () => {
     const token = localStorage.getItem("token");
 
-    const post = {
-      author: this.props.username,
-      title: this.state.title,
-      content: this.state.content,
-      tags: this.props.tagReducer.tags || []
-    };
-    this.setState({ loading: true });
+      const post = {
+        author: this.props.username,
+        title: this.state.title,
+        content: this.state.content,
+        tags: this.props.tagReducer.tags || []
+      };
 
-    const headers = {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Token ${token}`
-      }
-    };
+      this.setState({ loading: true });
 
-    axios
-      .post("/api/posts", post, headers)
-      .then(res => {
-        axios
-          .post(
-            `/api/users/myPosts/${this.props.userID}`,
-            { postID: res.data._id },
-            headers
-          )
-          .then(res => {
-            //cannot divide the call into two setState calls
-            this.setState({ loading: false, posted: true });
-          })
-          .catch(err => {
-            console.log(err);
-            this.setState({ loading: false });
-          });
-        var updatedMyPosts = [...this.props.myPosts];
-        updatedMyPosts.push(res.data._id);
-        var updatedMyPostsDetail = [...this.props.myPostsDetail]
-        updatedMyPostsDetail.push(res.data)
-        this.props.handleUpdatedMyPosts(updatedMyPostsDetail, updatedMyPosts);
-      })
-      .catch(err => {
-        this.setState({ loading: false });
+      const headers = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Token ${token}`
+        }
+      };
+
+      axios
+        .post("/api/posts", post, headers)
+        .then(res => {
+          axios
+            .post(
+              `/api/users/myPosts/${this.props.userID}`,
+              { postID: res.data._id },
+              headers
+            )
+            .then(res => {
+              //cannot divide the call into two setState calls
+              this.setState({ loading: false, posted: true });
+            })
+            .catch(err => {
+              console.log(err);
+              this.setState({ loading: false });
+            });
+          var updatedMyPosts = [...this.props.myPosts];
+          updatedMyPosts.push(res.data._id);
+          var updatedMyPostsDetail = [...this.props.myPostsDetail]
+          updatedMyPostsDetail.push(res.data)
+          this.props.handleUpdatedMyPosts(updatedMyPostsDetail, updatedMyPosts);
+        })
+        .catch(err => {
+          this.setState({ loading: false });
+        });
+
+      this.props.handlePosted();
+  }
+
+  handlePostCheck = (e) => {
+    e.preventDefault();
+    if (this.props.tagReducer.tags.length === 0 || this.props.tagReducer.tags.length >= 4) {
+      this.setState({ 
+        ...this.state,
+        tagError: true 
       });
-
-    this.props.handlePosted();
+    } else {
+      this.handleFinalPost();
+    }
   }
 
   updateContent = value => {
@@ -96,7 +111,11 @@ class Publish extends Component {
   };
 
   onCloseModal = () => {
-    this.setState({ warning: false });
+    this.setState({ 
+      ...this.state,
+      warning: false,
+      tagError: false
+    });
   };
 
   handleTitle = e => {
@@ -206,7 +225,7 @@ class Publish extends Component {
             this.successPosted()
           ) : (
                 <React.Fragment>
-                  <form onSubmit={this.handlePost}>
+                  <form onSubmit={this.handlePostCheck}>
                     <label>Title</label>
                     <input
                       className="input is-rounded"
@@ -241,14 +260,14 @@ class Publish extends Component {
                         type="submit"
                       >
                         Post
-                  </button>
+                    </button>
                       <button
                         className="button is-primary level-item"
                         type="button"
                         onClick={this.handleCancel}
                       >
                         Cancel
-                  </button>
+                    </button>
                     </div>
                   </form>
                 </React.Fragment>
@@ -272,6 +291,25 @@ class Publish extends Component {
             >
               Okay, I Got It
             </Link>
+          </div>
+        </Modal>
+        <Modal
+          className="modal-lg"
+          open={this.state.tagError}
+          onClose={this.onCloseModal}
+          center
+        >
+          <div>
+            <h1>
+              <strong>Warning</strong>
+            </h1>
+            <p style={{ color: "red" }}>Please limit the number of the input tags from 1 to 3</p>
+            <button
+              className="button is-link"
+              onClick={this.onCloseModal}
+            >
+              Okay, I Got It
+            </button>
           </div>
         </Modal>
       </React.Fragment>
